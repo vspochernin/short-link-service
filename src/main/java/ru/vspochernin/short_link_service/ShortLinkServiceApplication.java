ea8d38d5890029dd6dc496b74eba0a06044004c7
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-22 18:26:08 +0300

    initial commit for code

---
+++ src/main/java/ru/vspochernin/short_link_service/ShortLinkServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.short_link_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortLinkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vspochernin/short_link_service/ShortLinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.short_link_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
