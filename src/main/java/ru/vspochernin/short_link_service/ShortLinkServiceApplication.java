a4c099528cb510775d8f25ee30d62ea65fd72eaf
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-23 15:30:06 +0300

    add postgres database

--- src/main/java/ru/vspochernin/short_link_service/ShortLinkServiceApplication.java
+++ src/main/java/ru/vspochernin/short_link_service/ShortLinkServiceApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortLinkServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortLinkServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(ShortLinkServiceApplication.class, args);
+        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!" + i);
+            Thread.sleep(300);
+        }
+    }
 }
